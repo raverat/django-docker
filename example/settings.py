b26a0649e4472fa6bd6bac49a748cd049b6b46c4
Ravera Thibault <thibault.ravera@go-text.me>
2019-01-07 11:01:11 +0100

    Use PyMySQL

--- example/settings.py
+++ example/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except Exception as e:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
